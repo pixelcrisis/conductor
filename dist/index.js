d159644ae42d76bf751065e523e7ba2ccc8f30e8
pixelcrisis <pxcrisis@gmail.com>
2025-12-29 15:40:55 -0700

    Actually toggleable map panning.

--- dist/index.js
+++ dist/index.js
@@ -503,20 +503,20 @@
 
   // plugins/panning.js
   var panning_default = (map) => {
-    let mod = window.Conductor;
-    let config = mod.config.panning;
-    if (!config.enable) return;
-    if (!config.area || !config.distance) return;
-    const Pan = (dX, dY) => {
-      window.__panning = setInterval(() => {
-        map.panBy([dX * config.distance, dY * config.distance]);
-      }, config.delay);
-    };
     map.on("mousemove", (e) => {
+      let mod = window.Conductor;
+      let config = mod.config.panning;
+      if (window.__panning) clearInterval(window.__panning);
+      if (!config.enable) return;
+      if (!config.area || !config.distance) return;
+      const Pan = (dX, dY) => {
+        window.__panning = setInterval(() => {
+          map.panBy([dX * config.distance, dY * config.distance]);
+        }, config.delay);
+      };
       let size = map.getCanvas().getBoundingClientRect();
       let lenX = e.point.x - size.left;
       let lenY = e.point.y - size.top;
-      if (window.__panning) clearInterval(window.__panning);
       if (lenY < config.area) Pan(0, -1);
       else if (lenY > size.height - config.area - 104) Pan(0, 1);
       else if (lenX < config.area) Pan(-1, 0);
